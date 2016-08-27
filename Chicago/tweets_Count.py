ce9f3c448184d33524781949412c975900ff9d13
Jun <jyn@illinois.edu>
2016-08-27 14:18:12 -0500

    new edits

--- Chicago/tweets_Count.py
+++ Chicago/tweets_Count.py
@@ -3,9 +3,9 @@ import numpy
 
 def calculate_tweetNumbers():
     print "calculating Tweet numbers of people ..."
-    mFile = open("chicago_2014_traj.txt","rb")
+    mFile = open("C:\\project\\sq_mobility\\chitown\\2014_all_chi_traj.txt","rb")
     
-    pFile = open("tweetCount_Chicago_2014.txt", "wb")
+    pFile = open("C:\\project\\sq_mobility\\chitown\\tweetCount_Chicago_2014.txt", "wb")
     
     for mLine in mFile:
         line = mLine.split(',')
